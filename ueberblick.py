e56f36a3ddcdaecf0e304e882d97afd24b43c0f2
Dongmo Satsa teddy Guiderian <teddydongmo@gmail.com>
2025-12-05 18:47:16 +0100

    Improve file operations and add overview function
    Refactor file handling in append function and add ueberblick function to provide an overview of operations.

--- ueberblick.py
+++ ueberblick.py
@@ -1,14 +1,20 @@
-# erstellung von der funktion überblick
 
+# zuerst Erstellung der ueberblickdatei
 def create():
     file=open("ueberblick.txt","a")
     if(file==None):
         print(" the open's file is failed")
-
+# implementierung der Funktion append , um neue Operation hinzuzufügen
 def append(nomber):
-    file = open("ueberblick.txt", "a+")
-    file.closed
-    vorlezte=file.readline()
+    file = open("ueberblick.txt", "r")
+    vorlezte = file.readlines()
+    file.close()
     file = open("ueberblick.txt", "w")
     file.write(nomber)
     file.writelines(vorlezte)
+    file.close()
+  # implementierung der funktion Überblick , um den Benutzer , eine Überblick über seine Operationen , zu verschaffen
+def ueberblick():
+    file = open("ueberblick.txt", "r")
+    uberblick = file.readlines()
+    return uberblick
